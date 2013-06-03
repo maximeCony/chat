9e8fc79df824df3a5309d09b2a11097de99b27e7
maximeCony <maxime.cony@gmail.com>
2013-06-02 18:05:48 -0700

    set up socket.io

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,39 @@
 require.config({
-  	paths: {
+	 shim: {
+		'socketio': {
+			exports: 'io'
+		}
+	},
+	 paths: {
     	jquery: 'vendor/jquery/jquery.min',
     	underscore: 'vendor/underscore-amd/underscore-min',
-    	backbone: 'vendor/backbone-amd/backbone-min'
+    	backbone: 'vendor/backbone-amd/backbone-min',
+    	socketio: '../socket.io/socket.io',
+    	backbone_iobind: 'vendor/backbone.iobind/dist/backbone.iobind.min',
+    	backbone_iosync: 'vendor/backbone.iobind/dist/backbone.iosync.min'
 	}
 });
 
 require([
   // Load our app module and pass it to our definition function
   'app',
-], function(App){
-  // The "app" dependency is passed in as "App"
-  App.initialize();
+   'socketio'
+], function(App, io){
+
+	window.socket = io.connect('http://localhost');
+	/*
+	*   Log sockets
+	*/
+	var x = socket.$emit;
+	socket.$emit = function(){
+		var event = arguments[0];
+		var feed  = arguments[1];
+		//Log
+		console.log(event + ":", feed);
+		//To pass listener  
+		x.apply(this, Array.prototype.slice.call(arguments));       
+	};
+
+	// The "app" dependency is passed in as "App"
+	App.initialize();
 });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express'),
-app = express();
+var express = require('express')
+,app = express()
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server);
 
 var appDir;
 
@@ -36,10 +38,42 @@ app.configure('production', function(){
 var router = require('./server/router')(app);
 router.start();
 
-//set port
-var port = process.env.PORT || 3000;
+//db object used to save messages
+var db = [
+/*
+    {_id:1, title: 'Server - New message', checked: false},
+    {_id:2, title: 'Server - New message 2', checked: true}
+*/
+];
+
+io.sockets.on('connection', function (socket) {
+
+    /**
+    * message:create
+    *
+    * called when we .save() our new message
+    *
+    * we listen on model namespace, but emit
+    * on the collection namespace
+    */
+    socket.on('message:create', function (data, callback) {
+        //set id
+        data._id = db.length + 1;
+        //add message to db
+        db.push(data);
+        //send message to the client
+        socket.emit('messages:create', data);
+        //send message to the other clients
+        socket.broadcast.emit('messages:create', data);
+        callback(null, data);
+
+        console.log('db', db);
+    });
+
+});
 
 //start listening
-app.listen(port, function() {
-	console.log("Listening on " + port);
+var port = process.env.PORT || 8080;
+server.listen(port, function() {
+    console.log("Listening on " + port);
 });
