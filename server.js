a74f4a1824c62af731b8cfb8d8da5046978a3f1c
maximeCony <maxime.cony@gmail.com>
2013-06-09 01:55:43 -0700

    heroku config

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ var express = require('express')
 
 var appDir;
 
+//heroku config
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 // configuration for all environments
 app.configure(function(){
 
-    //heroku config
-    io.configure(function () { 
-      io.set("transports", ["xhr-polling"]); 
-      io.set("polling duration", 10); 
-    });
+
 
     //used to parse forms
     app.use(express.bodyParser());
